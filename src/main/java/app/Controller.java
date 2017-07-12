b55d77a703c0c36dbc89d6861929f55c738e775f
Stas Latyshev <postal915@gmail.com>
2017-07-12 15:25:13 +0300

    Added collection

--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -1,16 +1,33 @@
 package app;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
     public TableView newsTV;
 
     @FXML
-    public TableColumn newsTC;
+    public TableColumn<News, String> newsTC;
 
+    ObservableList<News> fxCollections = FXCollections.observableArrayList();
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        newsTC.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        newsTV.setItems(fxCollections);
+    }
+
+    public void refresh(ActionEvent event) {
+
+    }
 }

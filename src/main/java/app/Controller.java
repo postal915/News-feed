2f9d7a8e3fb6c409e3137953a8209b464a522784
Stas Latyshev <postal915@gmail.com>
2017-07-12 14:51:49 +0300

    Added interface and class controller

---
+++ src/main/java/app/Controller.java
@@ -0,0 +1,16 @@
+package app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class Controller {
+
+    @FXML
+    public TableView newsTV;
+
+    @FXML
+    public TableColumn newsTC;
+
+
+}

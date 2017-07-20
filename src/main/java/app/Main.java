46672e55a986feaa0c4b0e80544db4a92a6b597a
Stas Latyshev <postal915@gmail.com>
2017-07-20 09:48:07 +0300

    update setTitle

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/GUI.fxml"));
-        primaryStage.setTitle("Video Downloader");
+        primaryStage.setTitle("News feed");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();

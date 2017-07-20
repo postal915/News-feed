865d1fd0a9fd70065c7c75673f00610ddee57676
Stas Latyshev <postal915@gmail.com>
2017-07-20 09:50:06 +0300

    update class News modification

--- src/main/java/app/News.java
+++ src/main/java/app/News.java
@@ -1,124 +1,41 @@
 package app;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
 public class News {
 
-    public News(){
-        headLine = "Тестовый заголовок";
-    }
-
-    private String newsItemId;
     private String headLine;
-    private String agency;
-    private String dateLine;
-    private String webURL;
-    private String caption;
-    private String image;
-    private String keywords;
+    private String photo;
+    private String thumb;
     private String story;
-    private String commentCountUrl;
-    private String commentFeedUrl;
-    private String related;
 
-    public StringProperty nameProperty() {
-        return new SimpleStringProperty(headLine);
-    }
-
-    public void setNewsItemId(String newsItemId) {
-        this.newsItemId = newsItemId;
+    public String getHeadLine() {
+        return headLine;
     }
 
     public void setHeadLine(String headLine) {
         this.headLine = headLine;
     }
 
-    public void setAgency(String agency) {
-        this.agency = agency;
+    public String getPhoto() {
+        return photo;
     }
 
-    public void setDateLine(String dateLine) {
-        this.dateLine = dateLine;
+    public void setPhoto(String photo) {
+        this.photo = photo;
     }
 
-    public void setWebURL(String webURL) {
-        this.webURL = webURL;
+    public String getThumb() {
+        return thumb;
     }
 
-    public void setCaption(String caption) {
-        this.caption = caption;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public void setKeywords(String keywords) {
-        this.keywords = keywords;
-    }
-
-    public void setStory(String story) {
-        this.story = story;
-    }
-
-    public void setCommentCountUrl(String commentCountUrl) {
-        this.commentCountUrl = commentCountUrl;
-    }
-
-    public void setCommentFeedUrl(String commentFeedUrl) {
-        this.commentFeedUrl = commentFeedUrl;
-    }
-
-    public void setRelated(String related) {
-        this.related = related;
-    }
-
-    public String getNewsItemId() {
-        return newsItemId;
-    }
-
-    public String getHeadLine() {
-        return headLine;
-    }
-
-    public String getAgency() {
-        return agency;
-    }
-
-    public String getDateLine() {
-        return dateLine;
-    }
-
-    public String getWebURL() {
-        return webURL;
-    }
-
-    public String getCaption() {
-        return caption;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public String getKeywords() {
-        return keywords;
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
     }
 
     public String getStory() {
         return story;
     }
 
-    public String getCommentCountUrl() {
-        return commentCountUrl;
-    }
-
-    public String getCommentFeedUrl() {
-        return commentFeedUrl;
-    }
-
-    public String getRelated() {
-        return related;
+    public void setStory(String story) {
+        this.story = story;
     }
 }
